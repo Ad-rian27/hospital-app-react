f956c2b750d520716c32ed400b6600c5990a1a15
Ad-rian27 <rian270627@gmail.com>
2026-07-10 18:56:53 +0530

    Integrated API

--- src/components/DischargePatient.jsx
+++ src/components/DischargePatient.jsx
@@ -5,7 +5,7 @@ const DischargePatient = () => {
     return (
         <div>
 <NavigationBar />
-            <div className="container">
+            <div className="container mt-5">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
@@ -16,7 +16,7 @@ const DischargePatient = () => {
                                 <input type="text" className="form-control" />
 
                             </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                                 <button className="btn btn-primary">Remove</button>
 
